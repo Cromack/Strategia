82c244828694338dbcaf37e1a50db4158d55eb09
Vili Saura <vili.saura@student.tut.fi>
2019-07-09 16:44:29 +0300

    Modified and moved the cursor code to camera. Cursor position to grid position transform now works

--- Code/Strategy/Assets/Scripts/Control/CameraControl.cs
+++ Code/Strategy/Assets/Scripts/Control/CameraControl.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraControl : MonoBehaviour
 {
     private float xAxis;
     private float yAxis;
+    private Camera cam;
     public float zAxis;
 
     //The speed at which the camera moves
@@ -14,12 +16,37 @@ public class CameraControl : MonoBehaviour
     public float minZoom = 1;
     public float maxZoom = 15;
 
+    //Do this better later (dynamically?)
+    public Tilemap map;
+
     // Use this for initialization
     void Start()
     {
-
+        cam = Camera.main;
     }
 
+    void OnGUI()
+    {
+        Vector3 point = new Vector3();
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+        
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        Vector3Int gridPosition = map.WorldToCell(point);
+
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.Label("Grid position: " + gridPosition.ToString("F3"));
+        GUILayout.EndArea();
+    }
     // The camera movement is done with the input manager (WASD and arrow keys by default)
     void LateUpdate()
     {
