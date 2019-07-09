7f2029f45b91787dc42c165083871356e34183a5
Vili Saura <vili.saura@student.tut.fi>
2019-07-09 13:44:24 +0300

    Simple camera controls added. Testing with mouse positioning and controls

---
+++ Code/Strategy/Assets/Scripts/Control/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private float xAxis;
+    private float yAxis;
+    public float zAxis;
+
+    //The speed at which the camera moves
+    public float moveSpeed = 5;
+    public float scrollSpeed = 2;
+    public float minZoom = 1;
+    public float maxZoom = 15;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // The camera movement is done with the input manager (WASD and arrow keys by default)
+    void LateUpdate()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        zAxis = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += new Vector3(xAxis, yAxis, 0) * Time.deltaTime * moveSpeed;
+
+
+        Camera.main.orthographicSize -= zAxis * scrollSpeed;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
+    }
+}
