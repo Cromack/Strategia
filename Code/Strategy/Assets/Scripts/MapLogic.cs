5510ff9c8e291ab4a9e72618e6fb4935b67cf9d4
Vili Saura <vili.saura@student.tut.fi>
2019-12-26 14:47:17 +0200

    Testing is done, figured a better way to implement unit selection and movement

---
+++ Code/Strategy/Assets/Scripts/MapLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLogic : MonoBehaviour
+{
+
+    //Pitää kirjaa kartan tilasta, täyteyistä ruuduista yms
+    //Dicti "varatuista"ruuduista
+    //Database maatileistä täällä? vai erillinen pää database scripti?
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Map");
+        //Ilmoitetaan taistelulogiikalle että karttaa on painettu heksassa X
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Code/Strategy/Assets/Scripts/UnitLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitLogic : MonoBehaviour
+{
+    //Yksikköjen logiikka
+    //Hipat, liikkuminen voima yms, voidaan hakea jossain vaiheessa databasesta
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Unit");
+        //Ilmoitetaan battlelogikille mitä gameobjectia on klikattu (ja millä napilla)
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
